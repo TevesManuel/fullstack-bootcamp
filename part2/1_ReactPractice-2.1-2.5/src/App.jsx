eced91b8a15eadd7e73d95362ad4463d925344eb
Manuel Teves <manuel.tomas.teves@gmail.com>
2024-03-18 15:52:42 -0300

    Initializing part 2 & reorg

---
+++ part2/1_ReactPractice-2.1-2.5/src/App.jsx
@@ -0,0 +1,69 @@
+const Header = (params) => {
+  return (
+    <div>
+      <h1>{params.course}</h1>
+    </div>
+  );
+};
+const Part = (params) => {
+  return (
+    <div>
+      <p>
+        {params.part.name} {params.part.exercises}
+      </p>
+    </div>
+  );
+};
+const Content = (params) => {
+  return (
+    <div>
+      //https://reactjs.org/link/warning-keys
+      {params.parts.map(p => <Part key={self.crypto.randomUUID()} part={p}/>)}
+    </div>
+  );
+};
+const Total = (params) => {
+  let sum = 0;
+  params.parts.forEach(p => sum+=p.exercises);
+  return (
+    <div>
+        <p>Number of exercises {sum}</p>
+    </div>
+  );
+}
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </div>
+  );
+};
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
+
+  return (
+    <div>
+      <Course course={course}/>
+    </div>
+  );
+};
+
+export default App;
