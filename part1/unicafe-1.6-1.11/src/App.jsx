f7b0b5e2309ba9ad9e97fc26a364499e9ca8382d
Manuel Teves <manuel.tomas.teves@gmail.com>
2024-03-18 12:46:59 -0300

    Problem 1.8 solved.

--- part1/unicafe-1.6-1.11/src/App.jsx
+++ part1/unicafe-1.6-1.11/src/App.jsx
@@ -14,23 +14,30 @@ const Button = ({callback, text}) => {
     </div>
   );
 };
+const Statistics = ({good, neutral, bad}) => {
+  let all = good + neutral + bad;
+  return (
+    <div>
+      <Display name="all" content={all}/>
+      <Display name="average" content={(good-bad)/all}/>
+      <Display name="positive" content={good/all*100 + "%"}/>
+    </div>
+  );
+}
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [all, setAll] = useState([]);
 
   return (
     <div>
       <Display name="good" content={good}/>
       <Display name="neutral" content={neutral}/>
       <Display name="bad" content={bad}/>
-      <Display name="all" content={all.length}/>
-      <Display name="average" content={(good-bad)/all.length}/>
-      <Display name="positive" content={good/all.length*100 + "%"}/>
-      <Button callback={()=>{setAll(all.concat("good"));setGood(good + 1)}} text="good"/>
-      <Button callback={()=>{setAll(all.concat("neutral"));setNeutral(neutral + 1)}} text="neutral"/>
-      <Button callback={()=>{setAll(all.concat("bad"));setBad(bad + 1)}} text="bad"/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+      <Button callback={()=>{setGood(good + 1)}} text="good"/>
+      <Button callback={()=>{setNeutral(neutral + 1)}} text="neutral"/>
+      <Button callback={()=>{setBad(bad + 1)}} text="bad"/>
     </div>
   );
 };
