21a9cab7a33e972a8baa59407c5f64b717393d44
Manuel Teves <manuel.tomas.teves@gmail.com>
2024-03-18 16:15:45 -0300

    Problem 2.4 solved.

--- part2/1_ReactPractice-2.1-2.5/src/App.jsx
+++ part2/1_ReactPractice-2.1-2.5/src/App.jsx
@@ -17,7 +17,7 @@ const Part = (params) => {
 const Content = (params) => {
   return (
     <div>
-      //https://reactjs.org/link/warning-keys
+      {/* https://reactjs.org/link/warning-keys} */}
       {params.parts.map(p => <Part key={self.crypto.randomUUID()} part={p}/>)}
     </div>
   );
@@ -25,7 +25,7 @@ const Content = (params) => {
 const Total = (params) => {
   return (
     <div>
-        <p>Number of exercises {params.parts.reduce((acc, current) => acc+current.exercises, 0)}</p>
+        <h3>Number of exercises {params.parts.reduce((acc, current) => acc+current.exercises, 0)}</h3>
     </div>
   );
 }
@@ -39,27 +39,57 @@ const Course = ({course}) => {
   );
 };
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  };
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ];
+
 
   return (
     <div>
-      <Course course={course}/>
+      <ul>
+      {courses.map(course => <li key={course.id}><Course course={course}/></li>)}
+      </ul>
     </div>
   );
 };
