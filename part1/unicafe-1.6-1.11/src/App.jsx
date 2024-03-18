6c2a04c6649eb3c3aa4f9ab37f096469528272fb
Manuel Teves <manuel.tomas.teves@gmail.com>
2024-03-18 11:43:19 -0300

    Problem 1.7 solved.

--- part1/unicafe-1.6-1.11/src/App.jsx
+++ part1/unicafe-1.6-1.11/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 
-const Display = ({content}) => {
+const Display = ({name, content}) => {
   return (
     <div>
-      <p>{content}</p>
+      <p>{name}: {content}</p>
     </div>
   );
 };
@@ -18,15 +18,19 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState([]);
 
   return (
     <div>
-      <Display content={good}/>
-      <Display content={neutral}/>
-      <Display content={bad}/>
-      <Button callback={()=>{setGood(good + 1)}} text="good"/>
-      <Button callback={()=>{setNeutral(neutral + 1)}} text="neutral"/>
-      <Button callback={()=>{setBad(bad + 1)}} text="bad"/>
+      <Display name="good" content={good}/>
+      <Display name="neutral" content={neutral}/>
+      <Display name="bad" content={bad}/>
+      <Display name="all" content={all.length}/>
+      <Display name="average" content={(good-bad)/all.length}/>
+      <Display name="positive" content={good/all.length*100 + "%"}/>
+      <Button callback={()=>{setAll(all.concat("good"));setGood(good + 1)}} text="good"/>
+      <Button callback={()=>{setAll(all.concat("neutral"));setNeutral(neutral + 1)}} text="neutral"/>
+      <Button callback={()=>{setAll(all.concat("bad"));setBad(bad + 1)}} text="bad"/>
     </div>
   );
 };
