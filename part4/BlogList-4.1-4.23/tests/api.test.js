aaa54988e7312e7fff2f8207de46c2ccea5e5a35
TevesManuel <manuel.tomas.teves@gmail.com>
2024-04-08 18:18:35 -0300

    Switch device

--- part4/BlogList-4.1-4.23/tests/api.test.js
+++ part4/BlogList-4.1-4.23/tests/api.test.js
@@ -2,6 +2,7 @@ const mongoose  = require('mongoose');
 const supertest = require('supertest');
 const app       = require('./../src/app');
 const BlogModel = require('./../src/models/Blog');
+const blogController = require('./../src/controllers/Blog');
 const helper    = require('./helper');
 const api       = supertest(app);
 
@@ -25,12 +26,33 @@ describe('GET Blog API', () => {
     });
 
     test('blogs are returned with id and without _id', async () => {
-        let blogs = helper.blogsInDb();
-        console.log(blogs);
+        let blogs = await helper.blogsInDb();
         for(let blog of blogs)
         {
-            expect(blog._id).toBeDefined();
+            expect(blog.id).toBeDefined();
         }
+    });
+
+});
+
+describe('POST Blog API', () => {
+
+    test('Cheking of the creation of an user', async () => {
+        let new_blog_request = new BlogModel({
+            'title': 'NewBlog',
+            'author': 'AuthorOfBlog',
+            'url': 'BLOG_URL',
+            'likes': 5,
+        });
+        let countOfBlogs = (await helper.blogsInDb()).length;
+        let new_blog_response = await blogController.create(new_blog_request);
+        expect((await helper.blogsInDb()).length).toBeGreaterThan(countOfBlogs);
+        new_blog_response.__v = undefined;
+        new_blog_response._id = '';
+        new_blog_request._id  = '';
+        console.log('req', new_blog_response);
+        console.log('req', new_blog_request);
+        expect(new_blog_response).toContainEqual(new_blog_request);
     });
 
 });
