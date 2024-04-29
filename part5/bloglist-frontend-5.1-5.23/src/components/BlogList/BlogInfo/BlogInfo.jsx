0f0eb139f9f4dac13923e43860fdff5bfee537b1
U-WIN10-1909\Win10 1909 <manuel.tomas.teves@gmail.com>
2024-04-29 11:55:18 -0300

    swd

--- part5/bloglist-frontend-5.1-5.23/src/components/BlogList/BlogInfo/BlogInfo.jsx
+++ part5/bloglist-frontend-5.1-5.23/src/components/BlogList/BlogInfo/BlogInfo.jsx
@@ -1,9 +1,13 @@
 const BlogInfo = ({ blog }) => {
     return (
-        <div>
+        <div style={{ backgroundColor: 'rgba(255, 255, 255)', width: '30vw', padding:'5%' }}>
             <p>{blog.title}</p>
             <p>{blog.author}</p>
             <p>{blog.url}</p>
+            {/* <svg className='likeSVG' viewBox='0 0 270 270' width='270' height='270' preserveAspectRatio="xMidYMid meet" xmlns='./../public/svg/like.svg'></svg> */}
+            {/* <svg className='likeSVG' xmlns='./../public/svg/like.svg'></svg> */}
+            {/* <object className='likeSVG' data='./../public/svg/like.svg' type='image/svg+xml'></object> */}
+            <object className='likeSVG' data='./../public/svg/like.svg'></object>
         </div>
     );
 };
