f279fb73925926c53f731c8a8872b4170522b64b
Manuel Teves <manuel.tomas.teves@gmail.com>
2024-12-31 15:40:19 -0300

    8.2 resolved

--- part8/8.1-8.7/src/app.js
+++ part8/8.1-8.7/src/app.js
@@ -94,9 +94,16 @@ let books = [
 ]
 
 const typeDefs = `
+  type Book {
+    title: String!
+    author: String!
+    published: Int!
+    genres: [String!]!
+  }
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
@@ -104,6 +111,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
   }
 }
 
