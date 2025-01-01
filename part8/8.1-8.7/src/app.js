51e8820c1ad2e59e2d69ae002d8b192a88918635
Manuel Teves <manuel.tomas.teves@gmail.com>
2025-01-01 06:30:51 -0300

    8.6 solved

--- part8/8.1-8.7/src/app.js
+++ part8/8.1-8.7/src/app.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -113,6 +114,15 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -128,6 +138,15 @@ const resolvers = {
       return returnedBooks;
     },
     allAuthors: () => authors,
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() };
+      books = books.concat(book);
+      if(authors.filter((author) => author.name === book.author).length === 0)
+        authors = authors.concat({ name: book.author, id: uuid() });
+      return book;
+    }
   },
   Author: {
     bookCount: (root) => {
